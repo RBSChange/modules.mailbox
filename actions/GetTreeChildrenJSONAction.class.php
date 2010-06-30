55f6fc3c95e0f3aca11cf0a8cc803e89f6df3833
Eric HAUSWALD <eric.hauswald@rbs.fr>
2010-06-30 09:40:02 +0200

    [FIX] Suppression d'information de log invalide

--- actions/GetTreeChildrenJSONAction.class.php
+++ actions/GetTreeChildrenJSONAction.class.php
@@ -15,19 +15,23 @@ class mailbox_GetTreeChildrenJSONAction extends generic_GetTreeChildrenJSONActio
 		if ($document instanceof generic_persistentdocument_folder)
 		{
 			$dateLabel = $document->getLabel();
-			$matches = null;
-							Framework::fatal($dateLabel);
-			
+			$matches = null;			
 			if (preg_match('/^\d{4}\-\d{2}\-\d{2}$/', $dateLabel, $matches))
 			{
 				$startdate = date_Converter::convertDateToGMT($matches[0] . ' 00:00:00');
 				$endate = date_Calendar::getInstance($startdate)->add(date_Calendar::DAY, 1)->toString();
 				$offset = $this->getStartIndex();
 				$pageSize = $this->getPageSize();
-				$countQuery = mailbox_MessageService::getInstance()->createQuery()->add(Restrictions::between('creationdate', $startdate, $endate))->setProjection(Projections::rowCount('countItems'));
+				$countQuery = mailbox_MessageService::getInstance()->createQuery()->add(Restrictions::between('creationdate', $startdate, $endate))
+					->setProjection(Projections::rowCount('countItems'));
 				$resultCount = $countQuery->find();
 				$this->setTotal(intval($resultCount[0]['countItems']));
-				$query = mailbox_MessageService::getInstance()->createQuery()->add(Restrictions::between('creationdate', $startdate, $endate))->addOrder(Order::desc('document_creationdate'))->setFirstResult($offset)->setMaxResults($pageSize);
+				
+				$query = mailbox_MessageService::getInstance()->createQuery()
+					->add(Restrictions::between('creationdate', $startdate, $endate))
+					->addOrder(Order::desc('document_creationdate'))
+					->setFirstResult($offset)
+					->setMaxResults($pageSize);
 				return $query->find();
 			}
 		}
